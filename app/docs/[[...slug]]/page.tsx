201bad0fa86d97a63d295d2ab751a2d6ce0e9041
Dan Costello <dan@userclouds.com>
2025-05-09 14:21:37 -0700

    Styles

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -19,10 +19,15 @@ export default async function Page(props: {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
-      <DocsTitle>{page.data.title}</DocsTitle>
+    <DocsPage 
+      toc={page.data.toc} 
+      full={page.data.full}
+      tableOfContent={{
+        style: 'clerk',
+      }}>
+      <DocsTitle className="font-normal border-b border-fd-muted-foreground pb-2">{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
-      <DocsBody>
+      <DocsBody className="prose dark:prose-invert">
         <MDXContent
           components={getMDXComponents({
             // this allows you to link to other pages with relative file paths
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -11,15 +12,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image src="/logo.png" width={160} height={22} alt="UserClouds logo" className="dark:saturate-200"/>
       </>
     ),
   },
