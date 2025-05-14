3a7a0983035fe3e70ba2fd60fbfa405904debad0
Dan Costello <dan@userclouds.com>
2025-05-14 16:38:08 -0700

    Static

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV === 'development';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     unoptimized: isDev,
     remotePatterns: [
