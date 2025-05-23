49ac483aeed4cdd7cdf0fdce1bd508dd22b0bb10
Dan Costello <dan@userclouds.com>
2025-05-23 16:27:56 -0700

    Update file structure

--- scripts/build-openapi-pages.mjs
+++ scripts/build-openapi-pages.mjs
@@ -1,12 +1,10 @@
 import { generateFiles } from 'fumadocs-openapi';
 void generateFiles({
   // the OpenAPI schema
-  input: ['./**/openapi/**.yaml', './**/openapi/**.json'],
+  input: ['./**/content/openapi/**.yaml', './**/content/openapi/**.json'],
     // the output directory
-  output: './content/reference',
-  // we recommend to enable it
-  // make sure your endpoint description doesn't break MDX syntax.
+  output: './content/docs/reference/(generated)',
   includeDescription: true,
-  groupByFolder: true,
-  groupBy: true
+  per: "operation",
+  groupBy: "route"
 });
