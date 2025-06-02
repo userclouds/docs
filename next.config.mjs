63a1b13d6a629201b323e0acc935afe754a56fb3
Dan Costello <dan@userclouds.com>
2025-06-02 10:47:59 -0700

    Update build for pathless subdomain

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 import { createMDX } from "fumadocs-mdx/next";
 const withMDX = createMDX();
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const isProduction = process.env.NODE_ENV === "production";
 
 let assetPrefix = isProduction ? "./" : "";
 let basePath = "";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
 
 /** @type {import('next').NextConfig} */
 const config = {
