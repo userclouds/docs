8d7c24e79cf68183867846dfc1fe980668b2f6e0
Dan Costello <dan@userclouds.com>
2025-06-04 13:30:21 -0700

    Add announcement page (#15)

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,15 +1,23 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import type { ReactNode } from "react";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
+import { baseOptions } from "@/app/layout.config";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout 
-  {...baseOptions}
-    links= {[
-      {
-        text: 'Documentation',
-        url: '/docs',
-      }
-    ]}
-    >{children}</HomeLayout>
+  return (
+    <HomeLayout
+      {...baseOptions}
+      links={[
+        {
+          text: "Documentation",
+          url: "/docs",
+        },
+        {
+          text: "About",
+          url: "/about",
+        },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
