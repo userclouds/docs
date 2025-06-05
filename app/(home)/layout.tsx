198a5053b46b6d7e8eda6be0080cac6a28696971
Dan Costello <dan@userclouds.com>
2025-06-05 13:30:43 -0700

    Add golang metatags (#17)

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import type { ReactNode } from "react";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/app/layout.config";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "go-import": [
+      "userclouds.com git userclouds.com git https://github.com/userclouds/sdk-golang.git",
+    ],
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
